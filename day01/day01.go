b97b38726a710767726838e2db24ea8b65d75ca4
Teejizzm <teejizzm@gmail.com>
2023-12-01 17:44:29 +0000

    Day 01 - Part 1 complete in Go

--- day01/day01.go
+++ day01/day01.go
@@ -13,25 +13,60 @@ package main
 import (
 	// Input/Output
 	"fmt"
-	"io/ioutil"
+	"os"
+
 	// String manipulation
-	//"strings"
-	//"strconv"
+	"strconv"
+	"strings"
 	// Other required imports
 )
 
 /* -----------------------------------
 // Functions */
 
+func find_digits_in_string(line string) ([]int, []int) {
+
+	var digits []int
+	var indices []int
+
+	for i, char := range line {
+		if val, err := strconv.Atoi(string(char)); err == nil {
+			//fmt.Printf("%q looks like a number.\n", val)
+			indices = append(indices, i)
+			digits = append(digits, val)
+		}
+	}
+	return indices, digits
+}
+
+func part_one(lines []string) int {
+	var total int = 0
+
+	for _, line := range lines {
+
+		_, digits := find_digits_in_string(line)
+		total += (digits[0] * 10) + digits[len(digits)-1]
+	}
+
+	return total
+}
+
+func part_two(lines []string) int {
+	var part2 int = 0
+
+	return part2
+}
+
 func main() {
-	fmt.Println("Day 01 - *NAME*")
+	fmt.Println("Day 01 - Trebuchet?!")
 
 	// ------------ File -------------
 
-	//*/ - File switch
-	file, err := ioutil.ReadFile("day01/inc/ex.txt")
+	/*/ - File switch
+	file, err := os.ReadFile("day01/inc/ex1.txt")
+	//file, err := os.ReadFile("day01/inc/ex2.txt")
 	/*/ //
-	file, err := ioutil.ReadFile("day01/inc/in.txt")
+	file, err := os.ReadFile("day01/inc/in.txt")
 	// --- End --- */
 
 	if err != nil {
@@ -42,9 +77,10 @@ func main() {
 	//fmt.Print(text)
 
 	// ----------- Setup -------------
+	lines := strings.Split(text, "\n")
 
-	part1 := ""
-	part2 := ""
+	part1 := part_one(lines)
+	part2 := part_two(lines)
 
 	// ----------- Output ------------
 
