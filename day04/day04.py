dfd31184e2c4dc73522b4d0851f41c03c1208c43
Teejizzm <teejizzm@gmail.com>
2023-12-04 12:19:55 +0000

    Day 04 - Part 1 complete in Python

--- day04/day04.py
+++ day04/day04.py
@@ -12,6 +12,8 @@ import os
 
 import tools.texttolists as tl
 
+import re
+
 ############################
 # Variables
 
@@ -20,13 +22,32 @@ import tools.texttolists as tl
 ############################
 # Functions
 
+def part_one(winning, playing):
+
+    count = 0
+
+    for num in playing:
+        if num in winning:
+            count += 1
+
+    if count == 0: return 0
+    return pow(2, count -1)
+
 def day04(text):
-    print("Day 04 - *NAME*")
+    print("Day 04 - Scratchcards")
     
-    part1, part2 = text, ''
+    part1, part2 = 0, 0
     
-    
-    
+    cards = tl.to2dLists(text, "\n", ":")
+    for _, card in cards:
+
+        winning = re.findall(r"\d+", card.split("|")[0])
+        playing = re.findall(r"\d+", card.split("|")[1])
+        #print(winning)
+        #print(playing)5
+
+        part1 += part_one(winning, playing)
+
     return part1, part2
 
 ############################
@@ -37,8 +58,8 @@ if __name__ == "__main__":
     
     # Change file
     #######
-    file = "ex.txt"
-    #file = "in.txt"
+    #file = "ex.txt"
+    file = "in.txt"
     #######
     
     # Get absolute filepath of file
