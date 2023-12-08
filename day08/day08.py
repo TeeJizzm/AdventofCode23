6f5090c8efa5ea24bb20923d8659b5d9fe225e51
Teejizzm <teejizzm@gmail.com>
2023-12-08 13:05:01 +0000

    Day 08 - Part 1 complete in Python

--- day08/day08.py
+++ day08/day08.py
@@ -12,6 +12,10 @@ import os
 
 import tools.texttolists as tl
 
+from itertools import cycle
+import pandas as pd
+import re
+
 ############################
 # Variables
 
@@ -20,13 +24,29 @@ import tools.texttolists as tl
 ############################
 # Functions
 
+def part_one(instructions, df) -> int:
+
+    curr_pos = "AAA"
+    end_pos = "ZZZ"
+    for i, instr in enumerate(cycle(instructions)):
+        if curr_pos == end_pos:
+            return i
+        curr_pos = df.at[curr_pos, instr]
+
+    return -1
+
 def day08(text):
     print("Day 08 - Haunted Wasteland")
     
-    part1, part2 = text, ''
-    
-    
-    
+    instructions, nodetext = tl.to2dLists(text, "\n\n", "\n")
+
+    df = pd.DataFrame([re.findall(r"\w\w\w", each) for each in nodetext],
+                      columns=["pos", "L", "R"])
+    df.set_index("pos", inplace=True)
+
+    part1 = part_one(instructions[0], df)
+    part2 = 0
+
     return part1, part2
 
 ############################
@@ -37,8 +57,8 @@ if __name__ == "__main__":
     
     # Change file
     #######
-    file = "ex.txt"
-    #file = "in.txt"
+    #file = "ex.txt"
+    file = "in.txt"
     #######
     
     # Get absolute filepath of file
