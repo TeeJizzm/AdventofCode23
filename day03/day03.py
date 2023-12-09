0dcf2bdbc31745c3aee35fa0cd1a191c57a05ef9
TeeJizzm <teejizzm@gmail.com>
2023-12-09 11:57:29 +0000

    Day 03 - padding input

--- day03/day03.py
+++ day03/day03.py
@@ -17,7 +17,18 @@ import re
 ############################
 # Variables
 
+class num:
+    def __init__(self, val, row, col) -> None:
+        self.val = int(val)
+        self.row = int(row)
+        self.col_s = int(col)
+        self.col_e = self.col_s + len(str(self.val))
 
+class symbol:
+    def __init__(self, sym, row, col) -> None:
+        self.sym = sym
+        self.row = int(row)
+        self.col = int(col)
 
 ############################
 # Functions
@@ -30,12 +41,30 @@ def part_two():
 
     return 0
 
+def pad_input(grid, padding="."):
+    
+    padded_grid = [f"{padding}{row}{padding}" for row in grid]
+    padded_grid.insert(0, padding * len(padded_grid[0]))
+    padded_grid.append(padding * len(padded_grid))
+
+    return padded_grid
+
+
+
 def day03(text):
     print("Day 03 - Gear Ratios")
     
     part1, part2 = text, ''
     
-    rows = tl.toList(text)
+    rows = pad_input(tl.toList(text))
+    
+    print(rows)
+    nums =[]
+
+    for row_num, row in enumerate(rows):
+        for m in re.finditer(r"\d+", row):
+            nums.append(num(int(m.group[0]), row_num, m.start(0)))
+        
 
     return part1, part2
 
