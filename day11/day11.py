c4c495123a276fc37a064f6ddebbe4e733977203
Teejizzm <teejizzm@gmail.com>
2023-12-12 16:42:35 +0000

    Day 11 - Complete in Python

--- day11/day11.py
+++ day11/day11.py
@@ -12,21 +12,98 @@ import os
 
 import tools.texttolists as tl
 
+from itertools import combinations as combos
+
 ############################
 # Variables
 
-
+class galaxy:
+    def __init__(self, row, col) -> None:
+        self.row = int(row)
+        self.col = int(col)
+        self.dists = []
+    def add_dist(self, dist) -> None:
+        self.dists.append(dist)
 
 ############################
 # Functions
 
+def part_one(grid) -> int:
+
+    #exp_grid = grid_expansion(grid, ".")
+    e_rows, e_cols = find_expanded(grid, ".")
+    galaxies = find_galaxies(grid, "#", e_rows, e_cols)
+
+    for g1, g2 in list(combos(galaxies, 2)):
+        dist = abs(g1.row - g2.row) + abs(g1.col - g2.col)
+        g1.add_dist(dist)
+    
+    return sum([sum(gal.dists) for gal in galaxies])
+
+def part_two(grid) -> int:
+
+    e_rows, e_cols = find_expanded(grid, ".")
+    galaxies = find_galaxies(grid, "#", e_rows, e_cols, 1000000)
+
+    for g1, g2 in list(combos(galaxies, 2)):
+        dist = abs(g1.row - g2.row) + abs(g1.col - g2.col)
+        g1.add_dist(dist)
+    
+    return sum([sum(gal.dists) for gal in galaxies])
+
+def find_galaxies(grid, g, e_rows=[], e_cols=[], exp=2):
+
+    galaxies = []
+
+    for r, row in enumerate(grid):
+        for c, col in enumerate(row):
+            if col == g:
+                r_offset = sum(map(lambda e_r: r > e_r, e_rows)) * (exp-1)
+                c_offset = sum(map(lambda e_c: c > e_c, e_cols)) * (exp-1)
+                galaxies.append(galaxy(r_offset+r, c_offset+c))
+
+    return galaxies
+
+def grid_expansion(grid, s):
+
+    exp_grid = grid[:]
+
+    # Columns first
+    for col_index in reversed(range(len(grid[0]))):
+        if set([row[col_index] for row in grid]) == {s}:
+            for r in exp_grid:
+                r.insert(col_index, s)
+
+    # Next rows
+    for row_index, row in reversed(list(enumerate(grid))):
+        if set(row) == {s}:
+            exp_grid.insert(row_index, exp_grid[row_index])
+
+    return exp_grid
+
+def find_expanded(grid, s):
+    e_rows, e_cols = [], []
+
+    for c in range(len(grid[0])):
+        if set([row[c] for row in grid]) == {s}:
+            e_cols.append(int(c))
+    
+    for r, row in enumerate(grid):
+        if set(row) == {s}:
+            e_rows.append(int(r))
+
+    return e_rows, e_cols
+
 def day11(text):
-    print("Day 11 - *NAME*")
+    print("Day 11 - Cosmic Expansion")
     
-    part1, part2 = text, ''
+    part1, part2 = '', ''
     
-    
-    
+    grid = [[*row] for row in text.split("\n")]
+
+    part1 = part_one(grid)
+    part2 = part_two(grid)
+
     return part1, part2
 
 ############################
@@ -38,7 +115,7 @@ if __name__ == "__main__":
     # Change file
     #######
     file = "ex.txt"
-    #file = "in.txt"
+    file = "in.txt"
     #######
     
     # Get absolute filepath of file
